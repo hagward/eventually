66605849de80fedb89ac001ef69d82f3c1a33b35
Anders Hagward <taperlawyer@gmail.com>
2017-10-06 21:21:26 +0200

    Implement View component

--- src/View.js
+++ src/View.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
 export default class View extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:12345/api/event/' + this.props.match.params.id, {
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then(response => response.json())
+    .then(json => this.setState(json));
+  }
+
   render() {
     return (
-      <div>View</div>
+      <div>
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                View event
+              </h1>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <h1 className="title is-spaced">{this.state.title}</h1>
+          <h3 className="subtitle"><i className="fa fa-clock-o"></i> {this.state.time}</h3>
+          <p>{this.state.details}</p>
+        </section>
+      </div>
     );
   }
 }
